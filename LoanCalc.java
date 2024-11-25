1711f4fa45db3209cadc82d22f67edcfb81b5e42
Caroline Genzel <carolinegenzel@MacBook-Pro-sl-Caroline.local>
2024-11-25 23:07:55 +0200

    done

--- LoanCalc.java
+++ LoanCalc.java
@@ -17,7 +17,6 @@ public class LoanCalc {
 		// Computes the ending balance of the loan, given a periodical payment
 		double payment = 10000;
 		double endBalance = endBalance(loan, rate, n, payment);
-		System.out.println("If your periodical payment is " + payment + ", your ending balance is: " + (int)endBalance);		
 		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
